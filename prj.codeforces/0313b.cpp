56c15609d3c4b37911316fa80eb8804dda752bfc
AllaElisova <fluuffyer@yandex.ru>
2024-02-15 23:22:35 +0300

    upd 0313b.cpp

--- prj.codeforces/0313b.cpp
+++ prj.codeforces/0313b.cpp
@@ -1,6 +1,8 @@
 // not finished
 
-#include <iostream>
+// ver 1
+
+/* #include <iostream>
 #include <string>
 
 int main() {
@@ -29,5 +31,61 @@ int main() {
 			last = s.at(left);
 		}
 		std::cout << answer << std::endl;
+	}
+} */
+
+// ver 2
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+	std::string s;
+	getline(std::cin, s);
+
+	int number = 0;
+	std::cin >> number;
+
+	std::vector <int> data(number * 2);
+
+	for (int i = 0; i < number * 2 - 1; i += 2) {
+		int left;
+		int right;
+		std::cin >> left >> right;
+
+		left -= 1;
+		right -= 1;
+
+		data[i] = left;
+		data[i + 1] = right;
+	}
+
+	int left = data.at(0);
+	int right = data.at(1);
+
+	char last = s.at(left);
+	int answer = 0;
+	int counter = 0;
+
+	while (counter < number * 2) {
+			++left;
+			if (s.at(left) == last) {
+				++answer;
+			}
+			last = s.at(left);
+
+			if ((left == right) && (counter < number * 2 - 3)) {
+				std::cout << answer << std::endl;
+				counter += 2;
+				left = data.at(counter);
+				right = data.at(counter + 1);
+				last = s.at(left);
+				answer = 0;
+			}
+			else if ((left == right) && (counter >= number * 2 - 3)) {
+				std::cout << answer << std::endl;
+				counter += 10;
+			}
 	}
 }
